8a4c9540522d45a114ed7368dcbb101f1e44d489
SHUBHRO-JYOTI-DEY <deyshubhrojyoti@gmail.com>
2023-07-27 21:03:44 +0530

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -5,17 +5,17 @@ const bioData = [
         username: 'shubhro.links',
         url: 'https://shubhro-jyoti-dey.github.io/',
         titleImg: true,
-        avatar: '../public/avatar.png',
+        avatar: '/public/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'AI Engineer, Amateur Photographer, and a Finance student. Building and designing products for the Future!',
         descShow: true,
         subdesc: 'Use this template and deploy on vercel for free',
         subdescShow: true,
-        newProductUrl: 'https://3dicons.co',
+        newProductUrl: 'https://duo-studio.co',
         newProduct: true,
         author: 'shubhro jyoti dey',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
+        authorURL: 'https://twitter.com/shubhrojyoti1',
+        footerText: 'made with 🤍 by'
     },
 ];
 
