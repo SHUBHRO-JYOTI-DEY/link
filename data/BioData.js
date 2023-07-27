f052f4f5e32dd57eef473d7321542fb6bad52552
SHUBHRO-JYOTI-DEY <deyshubhrojyoti@gmail.com>
2023-07-27 20:37:31 +0530

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -5,7 +5,7 @@ const bioData = [
         username: 'shubhro.links',
         url: 'https://shubhro-jyoti-dey.github.io/',
         titleImg: true,
-        avatar: '/avatar.png',
+        avatar: 'public/avatar.png',
         nftAvatar: true,
         description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
         descShow: true,
